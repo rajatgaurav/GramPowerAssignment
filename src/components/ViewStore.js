b80fb85417189a676c665fc7ac7512d069ca8c66
RAJAT GAURAV <53166907+rajatgaurav@users.noreply.github.com>
2022-07-30 12:30:16 +0530

    Update ViewStore.js

--- src/components/ViewStore.js
+++ src/components/ViewStore.js
@@ -54,7 +54,7 @@ function ViewStore(props) {
           <h4>Location</h4>
           <p>{props.storeDetails.location}</p>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227748.99973450298!2d75.65047228361074!3d26.88514167956319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4adf4c57e281%3A0xce1c63a0cf22e09!2sJaipur%2C%20Rajasthan!5e0!3m2!1sen!2sin!4v1659024492208!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227748.99973450298!2d75.65047228361074!3d26.88514167956319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4adf4c57e281%3A0xce1c63a0cf22e09!2sDelhi%2C%20Rajasthan!5e0!3m2!1sen!2sin!4v1659024492208!5m2!1sen!2sin"
             width="400"
             height="200"
             title="map"
